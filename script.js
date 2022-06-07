3ba0dc122686d55d01e6944514082506ffef4abf
andrea scacchi <andrea.scacchi@live.com>
2022-06-07 10:02:10 +0200

    final commit, corrected exercise

--- script.js
+++ script.js
@@ -1,2 +1,21 @@
 // script for Exercise
 console.log("hello world");
+
+const myApp = new Vue ({
+    el: "#root",
+    data : {
+        emails: [],
+    },
+    mounted(){
+        for(let i=0; i<10; i++){
+            axios.get("https://flynn.boolean.careers/exercises/api/random/mail")
+            .then((result) => {
+                let risultato = result.data.response;
+                console.log(risultato);
+                this.emails.push(risultato);
+            });
+        }
+    }
+
+
+});
